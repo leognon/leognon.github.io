72e2fed13879c176b8aa82b9a164bdb00128d58f
Goel25 <leognon@gmail.com>
2017-10-26 21:08:58 -0400

    Added TO-DO list

--- sketch.js
+++ sketch.js
@@ -3,6 +3,9 @@ let projects;
 
 function setup() {
   data = loadJSON("data.json", loaded);
+  /*TODO (javascript to make the CSS better)
+    Make width of projects-container flexible with max between (100% - margin-width) and (width of each project)
+  */
 }
 
 function loaded() {
