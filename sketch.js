ea21fd6eb363b3718c482f6902d6958c61fb970a
Goel25 <leognon@gmail.com>
2017-10-29 09:41:45 -0400

    Updated TODO list

--- sketch.js
+++ sketch.js
@@ -4,8 +4,9 @@ let projects;
 function setup() {
   data = loadJSON("data.json", loaded);
   /*TODO (javascript to make the CSS better)
-    Make width of projects-container flexible with max between (100% - margin-width) and (width of each project)
-    Fix width of descBox to be flexible with img width and (.container width - .img width - .img padding - padding-left - padding right)
+    1. Make width of projects-container flexible with max between (100% - margin-width) and (width of each project)
+    2. Set min-width of projects-container to the width of a project
+    2. Fix width of descBox to be flexible with img width and (.container width - .img width - .img padding - padding-left - padding right)
   */
 }
 
